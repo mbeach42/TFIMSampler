f20bdc795de5a7a3303b896de015f730f6742c50
mbeach42 <mbeach@gra-login3.graham.sharcnet>
2019-09-09 11:12:16 -0400

    data

--- scripts/graham_script.sh
+++ scripts/graham_script.sh
@@ -2,12 +2,11 @@
 #SBATCH --account=rrg-rgmelko-ab
 #SBATCH --ntasks=1
 #SBATCH --mem=5GB
-#SBATCH --time=3:00:00
+#SBATCH --time=8:00:00
 #SBATCH --job-name=tfim_data
 #SBATCH --output=tfim_data.log
-#SBATCH --array=1-770%220
+#SBATCH --array=1-990%200
 
 module load nixpkgs/16.09 gcc/7.3.0 julia
 
-julia --project -O3 --check-bounds=no --math-mode=fast vita2d.jl
-$SLURM_ARRAY_TASK_ID 10000
+julia --project -O3 --check-bounds=no run_graham.jl $SLURM_ARRAY_TASK_ID 10000
--- scripts/run_graham.jl
+++ scripts/run_graham.jl
@@ -1,24 +1,25 @@
 using TFIMSampler
 
 N = 10000
-Ls = 2 .^ 2:10 |> collect
+Ls = 2 .^ collect(2:10)
 hs = 0.1*collect(1:20)
-append!(hs,[5.0, 10.0])
+append!(hs, [5.0, 10.0])
 repeats = [i for i in 1:5]
 
 arg = parse(Int, ARGS[1])
 
 list = Iterators.product(Ls, hs, repeats) |> collect
 println("length of array jobs is:\t", length(list))
+# display(list)
 
 L, h, r = list[arg]
-println("L is $L")
+println("\nL is $L")
 println("h is $h")
 println("r is $r")
 println("N is $N")
 
 # pre-compile once 
-@time configs, amps = run!(L=2, h=1.0, N=1)
+@time configs, amps = run!(L=1, h=1.0, N=1)
 
 # make directory if none exists
 dir = "../data/"
