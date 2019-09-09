8376c79154117fc5289fc24703e4b7eb15b57a3b
mbeach42 <mbeachphysics@gmail.com>
2019-09-09 15:51:24 -0400

    get_prob added

--- src/exact.jl
+++ src/exact.jl
@@ -1,4 +1,3 @@
-
 function exact_wf(L::Int; h=1.0)
     E, ψ = 0.0, zeros(L)
     for k in 0:div(L-1, 2)
@@ -28,11 +27,33 @@ function amplitudes(L=4, h=1)
         return C
     end
     C = buildC(L)[1:2:end, 2:2:end]
+    Z = det(I+C) #|> sqrt
     for i in 0:2^(L)-1 
         x = bitarray(i, L)
         f2 = C[x .> 0, x .> 0]
-        amp[i+1] = det(f2) 
+        amp[i+1] = det(f2) / Z
     end
-    normalize!(amp)
-    return amp
+    return C, amp
+end
+
+function get_prob(x::BitArray, L=4, h=1)
+    E = k -> sqrt(1 + h^2 + 2h*cospi((2k+1)/L))
+    Cf(n, k) = 2/L * (h*cospi((2k+1)*n/L)) / E(k) + 2/L * (cospi((2k+1)*(n+1)/L)) / E(k)
+    getC = n -> reduce(+, [Cf(n, k) for k in 0:div(L-1, 2)]) |> real
+    Π(n) = [0 getC(n); -getC(-n) 0]
+    function buildC(L)
+        C = zeros(2L, 2L)
+        for n in 0:L-1
+            Πs = map(n -> Π(n), -n:L-n-1)
+            Πs = hcat(Πs...)
+            C[2n+1:2n+2, :] = Πs
+        end
+        return C
+    end
+    C = buildC(L)[1:2:end, 2:2:end]
+    Z = det(I+C) #|> sqrt
+    x = bitarray(i, L)
+    f2 = C[x .> 0, x .> 0]
+    p = det(f2) / Z
+    return p
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,29 @@
 using TFIMSampler
 using Test
 
-@testset "TFIMSampler.jl" begin
+# @testset "TFIMSampler.jl" begin
 
-    h = 0.0
+    h = 0.4
 
-    amps_exact = amplitudes(4, h)
-    configs, amps = run!(L=4, h=h, N=100)
+    C, amps_exact = amplitudes(14, h)
+    # configs, amps = run!(L=4, h=h, N=100)
     # display(configs)
     # display(amps)
+    display(amps_exact)
+    println("Norm is :", norm(amps_exact, 1))
+    # display(normalize!(amps_exact))
+    # normalize!(amp)
     # display(amps_exact - amps)
     
-    diff = []
-    for N in 10 .^ [3,4,5]
-        @time configs, amps = run!(L=4, h=h, N=N)
-        display(amps)
-        # display(sum(abs.(amps_exact - amps)))
-        # append!(diff, sum(abs.(amps_exact - amps)))
-    end
-    display(diff)
+    # diff = []
+    # for N in 10 .^ [3,4,5]
+        # @time configs, amps = run!(L=4, h=h, N=N)
+        # display(amps)
+        # # display(sum(abs.(amps_exact - amps)))
+        # # append!(diff, sum(abs.(amps_exact - amps)))
+    # end
+    # display(diff)
 
-    @time configs, amps = run!(L=100, h=h, N=1000)
+    # @time configs, amps = run!(L=100, h=h, N=1000)
 
-end
+# end
