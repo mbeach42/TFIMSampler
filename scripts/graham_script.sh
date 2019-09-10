e39109bd7b001abc8339e633caff827c7d084e9a
mbeach42 <mbeach@gra-login3.graham.sharcnet>
2019-09-10 11:02:14 -0400

    cleaned up naming

--- scripts/graham_script.sh
+++ scripts/graham_script.sh
@@ -4,9 +4,9 @@
 #SBATCH --mem=5GB
 #SBATCH --time=24:00:00
 #SBATCH --job-name=tfim_data
-#SBATCH --array=1-30
+#SBATCH --array=1-900
 #SBATCH --output=tfim_data.log
 
 module load nixpkgs/16.09 gcc/7.3.0 julia
 
-julia --project -O3 --check-bounds=no run_graham.jl $SLURM_ARRAY_TASK_ID 10000
+julia --project -O3 --check-bounds=no run_graham.jl $SLURM_ARRAY_TASK_ID
--- scripts/run_graham.jl
+++ scripts/run_graham.jl
@@ -2,12 +2,12 @@ using TFIMSampler
 
 N = 10000
 # Ls = 2 .^ collect(2:10)
-Ls = 2 .^ collect(8:10)
+Ls = 2 .^ collect(2:10)
 # hs = 0.1*collect(1:10)
 # hs = [0.5, 1.0, 2.0]
 hs = [1.0]
 # append!(hs, [5.0, 10.0])
-repeats = [i for i in 1:10]
+repeats = [i for i in 1:100]
 # repeats = [i for i in 6:10]
 
 arg = parse(Int, ARGS[1])
@@ -27,8 +27,8 @@ println("N is $N")
 
 # make directory if none exists
 h = round(h, digits=2)
-dir = "../data/L-$L/h-$h/"
+dir = "/scratch/mbeach/tfim_data/L-$L/h-$h/"
 mkpath(dir)
-file = dir * "N-$N-r-$r" 
+file = dir * "r-$r" 
 
 @time configs, amps = run!(L=L, h=h, N=N, file=file)
