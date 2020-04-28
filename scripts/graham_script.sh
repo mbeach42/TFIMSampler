0e82d7ddea11a948875a1ef844143f1b4c4125b4
Matt <mbeachphysics@gmail.com>
2020-04-28 10:57:09 -0400

    num threads 1

--- scripts/graham_script.sh
+++ scripts/graham_script.sh
@@ -8,5 +8,8 @@
 #SBATCH --output=tfim_data.log
 
 module load nixpkgs/16.09 gcc/7.3.0 julia
-
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export JULIA_NUM_THREADS=1
 julia --project -O3 --check-bounds=no run_graham.jl $SLURM_ARRAY_TASK_ID
